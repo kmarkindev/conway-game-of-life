8a27851acf2aed83ecd1fc6ff0ebf8e396afa0da
Kirill Markin <kmarkin@mbsicorp.com>
2021-06-17 00:46:10 +0300

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,49 +2,32 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 #include "Renderer/RenderAPI.h"
-#include "Window.h"
-#include "Renderer/VertexBuffer.h"
-#include "Renderer/VertexArray.h"
-#include "Renderer/Shader.h"
-
-std::string vertextSrc = R"(
-#version 330 core
-
-layout (location = 0) in vec2 Vert;
-
-uniform mat4 matrix;
-
-void main()
-{
-    gl_Position = matrix * vec4(Vert.x, Vert.y, 0, 1);
-}
-)";
-
-std::string fragmentSrc = R"(
-#version 330 core
-
-out vec4 Color;
-
-void main()
-{
-    Color = vec4(1, 1, 1, 1);
-}
-)";
+#include "Renderer/FieldRenderer.h"
+#include "Renderer/Camera.h"
+#include "Time.h"
+#include "Random.h"
+#include "Field.h"
+#include "FieldSeeder.h"
 
 int main()
 {
     auto window = Window();
     auto renderApi = RenderAPI();
+    auto time = Time();
+    auto random = Random();
+    random.SetSeed(7788);
+    auto fieldSeeder = FieldSeeder(random);
+    auto camera = Camera();
+    auto fieldRenderer = FieldRenderer(&renderApi, &camera);
 
+    window.SetSize({800, 800});
     renderApi.SetViewport({0, 0, 800, 800});
-
     renderApi.SetClearColor({0.04f,0.00f,0.11f});
 
-    auto shader = Shader(vertextSrc, fragmentSrc);
-    auto vertexBuffer = VertexBuffer();
-    auto vertexArray = VertexArray();
+    auto field = Field({-10, 10, 10, -10});
+    fieldSeeder.ApplySeed(field);
 
-    vertexArray.AssignVertexBuffer(vertexBuffer);
+    time.ResetDeltaTime();
 
     while(!window.GetShouldClose())
     {
@@ -52,27 +35,9 @@ int main()
 
         renderApi.ClearColor();
 
-        shader.UseProgram();
+        fieldRenderer.Render(field);
 
-        auto proj = glm::ortho(-10.5f, 10.5f, 10.5f, -10.5f, 0.0f, 1.0f);
-
-        for(auto pos : {
-            glm::vec3({0, 0, 0}),
-            glm::vec3({1, 0, 0}),
-            glm::vec3({3, 2, 0}),
-            glm::vec3({1, -3, 0}),
-            glm::vec3({7, 3, 0}),
-            glm::vec3({-10, 10, 0})
-        })
-        {
-            auto transform = proj * glm::translate(glm::mat4(1.0f), pos);
-            shader.SetMat4("matrix", transform);
-
-            vertexArray.Bind();
-            renderApi.DrawArrays();
-            vertexArray.Unbind();
-        }
-
+        time.UpdateDeltaTime();
         window.SwapBuffers();
     }
 }
