4670ecb89a13b7a360b9001ed3d4f0e9c48ae786
Kirill Markin <kmarkin@mbsicorp.com>
2021-06-17 23:07:20 +0300

    Add UI

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,8 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 
 #include "Renderer/RenderAPI.h"
 #include "Renderer/FieldRenderer.h"
@@ -27,6 +30,12 @@ int main()
     auto camera = Camera();
     auto fieldRenderer = FieldRenderer(&renderApi, &camera);
 
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window.GetGlfwWindow(), true);
+    ImGui_ImplOpenGL3_Init(renderApi.GetOpenGlVersion().c_str());
+
     std::vector<std::shared_ptr<IRule>> rules = {
             std::make_shared<KillLessThanTwo>(),
             std::make_shared<KillMoreThanThree>(),
@@ -38,22 +47,63 @@ int main()
     renderApi.SetClearColor({0.04f,0.00f,0.11f});
 
     auto field = Field({-20, 20, 20, -20});
-    field.SetNoBounds(true);
     fieldSeeder.ApplySeed(field);
 
     time.ResetDeltaTime();
 
-    constexpr float timeoutLimit = 1.0f;
+    float timeoutLimit = 1.0f;
     float timeout = 0.0f;
+    bool shouldMakeSteps = false;
+    bool runOnce = false;
+    bool enableNoBoundsMode = false;
 
     while(!window.GetShouldClose())
     {
         window.PollEvents();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
 
-        renderApi.ClearColor();
+        // UI
 
-        timeout += time.GetDeltaTime();
-        if(timeout > timeoutLimit)
+        ImGui::SetNextWindowBgAlpha(0.5f);
+        ImGui::Begin("Menu");
+
+        if(ImGui::Checkbox("Enable no bounds mode##enablenoboundsmode", &enableNoBoundsMode))
+        {
+            field.SetNoBounds(enableNoBoundsMode);
+        }
+
+        if(ImGui::Checkbox("##shouldrunsteps", &shouldMakeSteps))
+        {
+            timeout = 0.0f;
+        }
+        ImGui::SameLine();
+        ImGui::Text("Time before step: %.2f / ", timeout);
+        ImGui::SameLine();
+        ImGui::PushItemWidth(40.0f);
+        ImGui::DragFloat("##timeoutlimit", &timeoutLimit, 0.01f, 0.0f, 0.0f, "%.2f");
+
+        ImGui::NewLine();
+
+        if(ImGui::Button("Next step"))
+        {
+            runOnce = true;
+        }
+
+        if(ImGui::Button("Generate new field"))
+        {
+            fieldSeeder.ApplySeed(field);
+        }
+
+        ImGui::End();
+
+        // Rules and Commands
+
+        if(shouldMakeSteps)
+            timeout += time.GetDeltaTime();
+
+        if(runOnce || (shouldMakeSteps && timeout > timeoutLimit))
         {
             timeout = 0.0f;
 
@@ -69,11 +119,21 @@ int main()
             {
                 command->ApplyTo(field);
             }
+
+            runOnce = false;
         }
 
+        ImGui::Render();
+
+        renderApi.ClearColor();
         fieldRenderer.Render(field);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         time.UpdateDeltaTime();
         window.SwapBuffers();
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 }
