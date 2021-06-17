acb82014336841174dfb1206a78d3f5e29ff9453
Kirill Markin <kmarkin@mbsicorp.com>
2021-06-17 20:04:49 +0300

    Fix rule

--- src/Rules/SpawnWithThree.cpp
+++ src/Rules/SpawnWithThree.cpp
@@ -4,35 +4,31 @@ std::vector<std::shared_ptr<ICommand>> SpawnWithThree::ApplyTo(Field& field)
 {
     auto commands = std::vector<std::shared_ptr<ICommand>>();
 
-    auto comp = [](const glm::ivec2& c1, const glm::ivec2& c2){
-        return (c1.x < c2.x && c1.y < c2.y);
-    };
+    std::map<std::pair<int, int>, int> deadCells;
 
-    std::map<glm::ivec2, int, decltype(comp)> deadCells;
-
-    for(auto iter = field.Begin(); iter != field.End(); ++iter)
+    for(auto cell = field.Begin(); cell != field.End(); ++cell)
     {
-        Cell cell = *iter;
-
-        for(glm::ivec2 pos : cell.GetNeighbors())
+        for(glm::ivec2 pos : cell->GetNeighbors())
         {
-            if(deadCells.contains(pos))
+            auto position = std::make_pair(pos.x, pos.y);
+            if(deadCells.contains(position))
             {
-                deadCells[pos] = deadCells[pos]++;
+                deadCells[position] = deadCells[position] + 1;
             }
             else
             {
-                deadCells.insert({pos, 1});
+                deadCells.insert({position, 1});
             }
         }
     }
 
     for(auto const& [pos, count] : deadCells)
     {
+        glm::ivec2 position = {std::get<0>(pos), std::get<1>(pos)};
         if(count == 3)
         {
             std::shared_ptr<ICommand> command =
-                    std::dynamic_pointer_cast<ICommand>(std::make_shared<SpawnCommand>(pos));
+                    std::dynamic_pointer_cast<ICommand>(std::make_shared<SpawnCommand>(position));
 
             commands.push_back(command);
         }
