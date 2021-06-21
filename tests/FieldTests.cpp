894c0b08b0c95376763216b3d14381d5da15e284
Kirill Markin <kmarkin@mbsicorp.com>
2021-06-21 20:58:33 +0300

    one more test

--- tests/FieldTests.cpp
+++ tests/FieldTests.cpp
@@ -19,3 +19,17 @@ TEST(FieldTests, RaiseExceptionWhenSpawnCellOutsideFieldWithNoBoundsMode)
     ASSERT_THROW(field.SpawnCell({-999, -999}), std::invalid_argument)
         << "Field didn't raise an exception when spawn a cell outside the field with disabled 'no bounds' mode";
 }
+
+TEST(FieldTests, CreateAndKillCellWithCountChecks)
+{
+    Field field({-10, 10, 10, -10});
+    field.SetNoBounds(false);
+
+    EXPECT_EQ(0, field.GetActiveCellsCount());
+    field.SpawnCell({0, 0});
+    EXPECT_EQ(1, field.GetActiveCellsCount());
+    ASSERT_TRUE(field.HasActiveCell({0, 0}));
+    field.KillCell({0, 0});
+    EXPECT_EQ(0, field.GetActiveCellsCount());
+    ASSERT_FALSE(field.HasActiveCell({0, 0}));
+}
